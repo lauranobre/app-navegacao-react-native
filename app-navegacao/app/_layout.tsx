c57429820ca20ecf34f27bb9a2c07f08cff17010
Laura Nobre <laura.a.nobre@aluno.senai.br>
2026-02-19 08:29:05 -0300

    Terceiro comit - reformulação das páginas

--- app-navegacao/app/_layout.tsx
+++ app-navegacao/app/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from 'expo-router';
 /*ESTOU DIZENDO PRA ELE QUE VOU CRIAR ESSAS DUAS TELAS*/
 export default function RootLayout() {
   return (
-    <Stack>
+    <Stack screenOptions={{ headerShown: false }} /*ESCONDE O CABEÇALHO PADRÃO DO EXPO ROUTER*/>
       <Stack.Screen name='index' options={{ title: 'Home' }} />
       <Stack.Screen name='detalhes' options={{ title: 'Detalhes' }} />
       <Stack.Screen name='sobre' options={{ title: 'Sobre' }} />
